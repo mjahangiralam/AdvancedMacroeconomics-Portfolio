2cdbf1a60c8674904089aa1b90554fd2332a6444
M. Jahangir Alam, PhD <138935490+mjahangiralam@users.noreply.github.com>
2026-08-25 17:37:03 -0400

    Add Waterloo Work Android activity

---
+++ waterloo-apk/app/src/main/java/org/aieconlab/waterloowork/MainActivity.java
@@ -0,0 +1,27 @@
+package org.aieconlab.waterloowork;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MainActivity extends Activity {
+  private WebView webView;
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    webView = new WebView(this);
+    setContentView(webView);
+    webView.getSettings().setJavaScriptEnabled(true);
+    webView.getSettings().setDomStorageEnabled(true);
+    webView.setWebViewClient(new WebViewClient());
+    webView.loadUrl("https://waterloo-work-mobile-ui-ai-econ-lab.vercel.app");
+  }
+
+  @Override
+  public void onBackPressed() {
+    if (webView != null && webView.canGoBack()) webView.goBack();
+    else super.onBackPressed();
+  }
+}
